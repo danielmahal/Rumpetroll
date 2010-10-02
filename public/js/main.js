161b726a00093024729c38465a0e4d934c77db99
Hans Petter Eikemo <hpeikemo@me.com>
2010-10-02 23:58:42 +0200

    Mention Firefox beta 4 in compatibility alert.

--- public/js/main.js
+++ public/js/main.js
@@ -6,10 +6,10 @@ var isStatsOn = false;
 try {
 	var ua = navigator.userAgent.toLowerCase();
 	if(ua.search('chrome') < 0 && ua.search('safari') < 0) {
-		alert('This is an experiment that has only been tested in Chrome and Safari, and may not work in other browsers');
+		alert('This is an experiment that has only been tested in Chrome, Safari and Firefox 4Beta, and may not work in other browsers');
 	}
 } catch(err) {
-	alert('This is an experiment that has only been tested in Chrome and Safari, and may not work in other browsers');
+	alert('This is an experiment that has only been tested in Chrome, Safari and Firefox 4Beta, and may not work in other browsers');
 }
 
 var runLoop = function() {
