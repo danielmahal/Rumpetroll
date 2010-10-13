1b97fad50a52d9a5d1a5cf9937b63dcb780c3e5c
Hans Petter Eikemo <hpeikemo@me.com>
2010-10-13 13:45:45 +0200

    Minor refactor. FPS counter on top of logo. app initialization wrapped in function.

--- public/js/main.js
+++ public/js/main.js
@@ -13,28 +13,33 @@ try {
 	alert(unsupportedAlert);
 }
 
+var app;
 var runLoop = function() {
 	app.update();
 	app.draw();
 }
+var initApp = function() {
+	app = new App(settings, document.getElementById('canvas'));
 
-var app = new App(settings, document.getElementById('canvas'));
+	window.addEventListener('resize', app.resize, false);
 
-window.addEventListener('resize', app.resize, false);
+	document.addEventListener('mousemove', 		app.mousemove, false);
+	document.addEventListener('mousedown', 		app.mousedown, false);
+	document.addEventListener('mouseup',			app.mouseup, false);
+	
+	document.addEventListener('touchstart',   app.touchstart, false);
+	document.addEventListener('touchend',     app.touchend, false);
+	document.addEventListener('touchcancel',  app.touchend, false);
+	document.addEventListener('touchmove',    app.touchmove, false);
 
-document.addEventListener('mousemove', app.mousemove, false);
-document.addEventListener('mousedown', app.mousedown, false);
-document.addEventListener('mouseup', app.mouseup, false);
+	setInterval(runLoop,30);
+}
 
-document.addEventListener('touchstart',   app.touchstart, false);
-document.addEventListener('touchend',     app.touchend, false);
-document.addEventListener('touchcancel',  app.touchend, false);
-document.addEventListener('touchmove',    app.touchmove, false);
+initApp();
 
-
-setInterval(runLoop,30);
 
 var addStats = function() {
+	if (isStatsOn) { return; }
 	// Draw fps
 	var stats = new Stats();
 	document.getElementById('fps').appendChild(stats.domElement);
@@ -49,19 +54,16 @@ var addStats = function() {
 	  array.length = from < 0 ? array.length + from : from;
 	  return array.push.apply(array, rest);
 	};
+	isStatsOn = true;
 }
 
 document.addEventListener('keydown',function(e) {
-	if(e.which == 27 && !isStatsOn) {
+	if(e.which == 27) {
 		addStats();
-		isStatsOn = true;
 	}
 })
 
-if(debug) {
-	addStats();
-	isStatsOn = true;
-}
+if(debug) { addStats(); }
 
 $(function() {
 	$('a[rel=external]').click(function(e) {
